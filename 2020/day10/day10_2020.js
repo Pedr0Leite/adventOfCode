6782ec82d2b8353d5687e5d1d4f2e35b9e7b7835
Pedro leite <pedromiguelgoncalvesleite@gmail.com>
2020-12-15 00:17:08 +0000

    day10 part1

--- 2020/day10/day10_2020.js
+++ 2020/day10/day10_2020.js
@@ -5,12 +5,57 @@ var day10 = require('./day10_2020_utils.js').day10;
 var day10_part2 = require('./day10_2020_utils.js').day10_part2;
 var data = read.toString().split("\r\n").map(x => +x); 
 var dataTest_p1 = readTest_p1.toString().split("\r\n").map(x => +x);
-console.log('dataTest_p1 :', dataTest_p1);
+// console.log('dataTest_p1 :', dataTest_p1);
 
 
 // //Part1
-console.log('PART1: ' + day10(dataTest_p1));
+// console.log('PART1: ' + day10(dataTest_p1));
 // console.log('PART1: ' + day10(data));
+// 
 //Part2
-// console.log('PART2: ' + day10_part2(dataTest_p1));
+console.log('PART2: ' + day10_part2(dataTest_p1));
 // console.log('PART2: ' + day10_part2(data));
+
+// 28
+// 33
+// 18
+// 42
+// 31
+// 14
+// 46
+// 20
+// 48
+// 47
+// 24
+// 23
+// 49
+// 45
+// 19
+// 38
+// 39
+// 11
+// 1
+// 32
+// 25
+// 35
+// 8
+// 17
+// 7
+// 9
+// 4
+// 2
+// 34
+// 10
+// 3
+
+// 16
+// 10
+// 15
+// 5
+// 1
+// 11
+// 7
+// 19
+// 6
+// 12
+// 4
--- 2020/day10/day10_2020_utils.js
+++ 2020/day10/day10_2020_utils.js
@@ -1,8 +1,45 @@
 const day10 = (data) => {
- 
+  data = data.sort((a,b)=> {return a - b});
+  var oneJolt = [];
+  var twoJolt = [];
+  var threeJolt = [];
+  var arrOfarrangements = [];
+  data.unshift(0);
+  data.push(data[data.length - 1] + 3)
+  // console.log('data :', data);
+  for (var i = 0; i < data.length; i++) {
+    // console.log(data[i]);
+    (data[i + 1] - data[i] == 1) ? (oneJolt.push(data[i])) : null;
+    (data[i + 1] - data[i] == 2) ? (twoJolt.push(data[i])) : null;
+    (data[i + 1] - data[i] == 3) ? (threeJolt.push(data[i])) : null;
+
+  }
+  console.log('oneJolt :', oneJolt.length);
+  console.log('twoJolt :', twoJolt.length);
+  console.log('threeJolt :', threeJolt.length);
+  return oneJolt.length * threeJolt.length
+  
 };
 
+//1890 -> Correct
+
 const day10_part2 = (data) => {
+  data = data.sort((a,b)=> {return a - b});
+  let dataTest = data.slice(0,6);
+  console.log('dataTest :', dataTest);
+  let tempArr_I = [];
+  let tempArr_II = [];
+  let tempArr_III = [];
+  let tempArr_IV = [];
+  let tempArr_V = [];
+  for (var i = 0; i < dataTest.length; i++) {
+    
+  }
+  console.log('tempArr_I :', tempArr_I);
+  console.log('tempArr_II :', tempArr_II);
+  console.log('tempArr_III :', tempArr_III);
+  console.log('tempArr_IV :', tempArr_IV);
+  console.log('tempArr_V :', tempArr_V);
   
 };
 
