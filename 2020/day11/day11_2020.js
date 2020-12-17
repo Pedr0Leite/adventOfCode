4e900ba43027e9a9830e1c05c9786a23516ac6b0
Pedro leite <pedromiguelgoncalvesleite@gmail.com>
2020-12-17 00:43:46 +0000

    day11 part1

--- 2020/day11/day11_2020.js
+++ 2020/day11/day11_2020.js
@@ -11,19 +11,3 @@ var dataTest = readTest.toString().split("\r\n");
 //Part 1
 // console.log(day11(dataTest));
 console.log(day11(data));
-
-
-
-//Part 2
-// console.log('result 2: ' + day11(data));
-// console.log('result 2_II: ' + day11(data));
-// L.LL.LL.LL
-// LLLLLLL.LL
-// L.L.L..L..
-// LLLL.LL.LL
-// L.LL.LL.LL
-// L.LLLLL.LL
-// ..L.L.....
-// LLLLLLLLLL
-// L.LLLLLL.L
-// L.LLLLL.LL
